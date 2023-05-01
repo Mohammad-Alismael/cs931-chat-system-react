39a84e9e7718ace25071bd75ace3e25efc575e5b
Mohammad-Alismael <mhdhd260@gmail.com>
2023-05-01 17:28:39 +0400

    adding HomePage.jsx screen

--- chat-system/src/App.jsx
+++ chat-system/src/App.jsx
@@ -3,45 +3,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import {
-  Button,
-  Card,
-  CardBody,
-  CardSubtitle,
-  CardText,
-  CardTitle,
-  Col,
-  Container,
-  Form, FormFeedback,
-  FormGroup, FormText, Input, Label,
-  Row
-} from 'reactstrap';
-
+import HomePage from "./pages/homePage/HomePage.jsx";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return (
-      <Container fluid>
-        <Container className="bg-light">
-          Sign Up form code goes here
-          <Form>
-            <FormGroup>
-              <Label for="exampleEmail">
-                Input without validation
-              </Label>
-              <Input />
-              <FormFeedback>
-                You will not be able to see this
-              </FormFeedback>
-              <FormText>
-                Example help text that remains unchanged.
-              </FormText>
-            </FormGroup>
-          </Form>
-        </Container>
-      </Container>
-  );
+  return <HomePage/>
 }
 
 export default App;
---
+++ chat-system/src/pages/homePage/HomePage.jsx
@@ -0,0 +1,66 @@
+import React, { useRef } from "react";
+import "./HomePage.styles.css";
+
+HomePage.propTypes = {};
+
+function HomePage(props) {
+  const containerRef = useRef(null);
+
+  const handleSignUpBtn = (e) => {
+    containerRef.current.classList.add("right-panel-active");
+  };
+
+  const handleSignInBtn = (e) => {
+    containerRef.current.classList.remove("right-panel-active");
+  };
+
+  return (
+    <div ref={containerRef} className="container" id="container">
+      <div className="form-container sign-up-container">
+        <form action="#">
+          <h1>Create Account</h1>
+          <span>or use your email for registration</span>
+          <input type="text" placeholder="Name" />
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <input type="password" placeholder="Confirm Password" />
+          <input type="textarea" placeholder="Tell Us More About Yourself" />
+          <button>Sign Up</button>
+        </form>
+      </div>
+
+      <div className="form-container sign-in-container">
+        <form action="#">
+          <h1>Sign in</h1>
+          <span>or use your account</span>
+          <input type="email" placeholder="Email" />
+          <input type="password" placeholder="Password" />
+          <a href="#">Forgot your password?</a>
+          <button>Sign In</button>
+        </form>
+      </div>
+      <div className="overlay-container">
+        <div className="overlay">
+          <div className="overlay-panel overlay-left">
+            <h1>Welcome Back!</h1>
+            <p>
+              To keep connected with us please login with your personal info
+            </p>
+            <button onClick={handleSignInBtn} className="ghost" id="signIn">
+              Sign In
+            </button>
+          </div>
+          <div className="overlay-panel overlay-right">
+            <h1>Hello, Friend!</h1>
+            <p>Enter your personal details and start journey with us</p>
+            <button onClick={handleSignUpBtn} className="ghost" id="signUp">
+              Sign Up
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HomePage;
