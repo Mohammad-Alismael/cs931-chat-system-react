1e639712e2b661707932581c260266586023665b
Mohammad-Alismael <mhdhd260@gmail.com>
2023-05-03 16:06:55 +0400

    adding user settings div

--- chat-system/src/pages/chatPage/components/ChatHeader.jsx
+++ chat-system/src/pages/chatPage/components/ChatHeader.jsx
@@ -1,20 +1,26 @@
 import React from "react";
 import Avatar from "../../../components/avatar/Avatar.jsx";
-import { BsSearch, BsThreeDotsVertical } from "react-icons/bs";
+import { BsSearch, BsThreeDotsVertical, BsCameraVideo } from "react-icons/bs";
+import {IoCallOutline} from "react-icons/all.js";
 
 function ChatHeader(props) {
   return (
     <div className="d-flex align-items-center justify-content-between mt-2 border-bottom pb-2">
       <div className="d-flex align-items-center">
         <Avatar imageUrl="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60" />
-        <div className="d-inline-block pl-2" style={{textAlign: 'left',paddingLeft: '1rem'}}>
-          <p className="p-0 m-0 text-left">Gavin Griffith</p>
+        <div
+          className="d-inline-block pl-2"
+          style={{ textAlign: "left", paddingLeft: "1rem" }}
+        >
+          <p className="p-0 m-0 text-left"><b>Gavin Griffith</b></p>
           <span className="p-0 m-0">online</span>
         </div>
       </div>
       <div className="d-flex gap-3">
-        <BsSearch size={23}/>
-        <BsThreeDotsVertical size={23}/>
+        <IoCallOutline size={23} />
+        <BsCameraVideo size={25} />
+        <BsSearch size={23} />
+        <BsThreeDotsVertical size={23} />
       </div>
     </div>
   );
--- chat-system/src/pages/chatPage/index.jsx
+++ chat-system/src/pages/chatPage/index.jsx
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
 import PropTypes from "prop-types";
 import {
+  Button,
   Col,
   Container,
+  FormGroup,
+  Input,
+  Label,
+  ListGroup,
+  ListGroupItem,
   Offcanvas,
   OffcanvasBody,
   OffcanvasHeader,
@@ -14,6 +20,8 @@ import ChatHeader from "./components/ChatHeader.jsx";
 import SendBar from "./components/SendBar.jsx";
 import styles from "./page.module.css";
 import ChatBody from "./components/chatBody/ChatBody.jsx";
+import Avatar from "../../components/avatar/Avatar.jsx";
+import {BiUser, FaEnvelope, FaUser, MdNotificationsNone, RiDeleteBin6Line} from "react-icons/all.js";
 
 ChatPage.propTypes = {};
 
@@ -21,7 +29,7 @@ function Chats() {
   return (
     <Row>
       <h4 className={styles.header}>Chats</h4>
-      <Col style={{height: '195px',overflowY: 'scroll'}}>
+      <Col style={{ height: "195px", overflowY: "scroll" }}>
         <UserChatCard />
         <UserChatCard />
         <UserChatCard />
@@ -37,7 +45,7 @@ function Contacts() {
   return (
     <Row>
       <h4 className={styles.header}>Contacts</h4>
-      <Col style={{height: '250px',overflowY: 'scroll'}}>
+      <Col style={{ height: "250px", overflowY: "scroll" }}>
         <UserChatCard />
         <UserChatCard />
         <UserChatCard />
@@ -50,28 +58,64 @@ function Contacts() {
 }
 
 function ChatPage(props) {
-  const [openUserProfile, setOpenUserProfile] = useState(false);
+  const [openUserProfile, setOpenUserProfile] = useState(true);
+  const openSettings = () => {
+    setOpenUserProfile(!openUserProfile);
+  };
   return (
     <Container>
       <Row>
         <Col xs={4} className="bg-light border">
-          <SearchBar />
+          <SearchBar openSettings={openSettings} />
           <Chats />
           <Contacts />
+          <Offcanvas isOpen={openUserProfile} toggle={openSettings}>
+            <OffcanvasHeader toggle={openSettings}>
+              User Settings
+            </OffcanvasHeader>
+            <OffcanvasBody>
+              <div>
+                <Avatar className="pr-2" imageUrl="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MXx8dXNlcnxlbnwwfHwwfHw%3D&auto=format&fit=crop&w=800&q=60" />
+                <p className='d-inline-block mx-3'><b>Gavin Griffith</b></p>
+                <FormGroup className="pt-3 pb-2">
+                  <Label for="exampleText">About</Label>
+                  <Input id="exampleText" name="text" type="textarea" />
+                </FormGroup>
+              </div>
+              <div>
+                <h5>Settings</h5>
+                <ListGroup flush>
+                  <ListGroupItem>
+                    <MdNotificationsNone size={25} className="mr-2" />
+                    <sapn className="px-2">Notification</sapn>
+                  </ListGroupItem>
+                  <ListGroupItem>
+                    <BiUser size={25} />
+                    <sapn className="px-2">Invite Friends</sapn>
+                  </ListGroupItem>
+                  <ListGroupItem>
+                    <RiDeleteBin6Line size={25} />
+                    <sapn className="px-2">Delete Account</sapn>
+                  </ListGroupItem>
+                </ListGroup>
+              </div>
+              <Button className="mt-4">Logout</Button>
+            </OffcanvasBody>
+          </Offcanvas>
         </Col>
         <Col xs={8} className="bg-light border">
           <Row>
-            <Col xs={12} style={{background: 'rgb(238, 238, 238)'}}>
+            <Col xs={12} style={{ background: "rgb(238, 238, 238)" }}>
               <ChatHeader />
             </Col>
           </Row>
           <Row>
-            <Col xs={12} style={{background: 'rgb(238, 238, 238)'}}>
+            <Col xs={12} style={{ background: "rgb(238, 238, 238)" }}>
               <ChatBody />
             </Col>
           </Row>
           <Row>
-            <Col xs={12} style={{background: 'rgb(238, 238, 238)'}}>
+            <Col xs={12} style={{ background: "rgb(238, 238, 238)" }}>
               <SendBar />
             </Col>
           </Row>
